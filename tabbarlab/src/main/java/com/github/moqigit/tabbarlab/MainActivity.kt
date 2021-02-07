b7f927b80cc158d647bef3caf4522825a537832d
moqi-Git <moqi.kaze@gmail.com>
2021-02-07 12:10:08 +0800

    [sync]

--- tabbarlab/src/main/java/com/github/moqigit/tabbarlab/MainActivity.kt
+++ tabbarlab/src/main/java/com/github/moqigit/tabbarlab/MainActivity.kt
@@ -2,6 +2,10 @@ package com.github.moqigit.tabbarlab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+import androidx.viewpager.widget.ViewPager
 import com.github.moqigit.tabbarlab.databinding.ActivityMainBinding
 import com.github.moqigit.tabbarlab.ui.SolutionA
 import com.github.moqigit.tabbarlab.ui.SolutionB
@@ -17,42 +21,84 @@ class MainActivity : AppCompatActivity() {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
 
+        val mTabIds = listOf(
+            R.id.navi_item_1,
+            R.id.navi_item_2,
+            R.id.navi_item_3,
+//            R.id.navi_item_4,
+        )
+
         vb.mainBnv.labelVisibilityMode = LabelVisibilityMode.LABEL_VISIBILITY_LABELED
         vb.mainBnv.setOnNavigationItemSelectedListener {
-            selectSolutionById(it.itemId)
+//            selectSolutionById()
+            vb.mainViewPager.setCurrentItem(mTabIds.indexOf(it.itemId), true)
             return@setOnNavigationItemSelectedListener true
         }
         vb.mainBnv.setOnNavigationItemReselectedListener {
             // refresh content fragment
         }
         vb.mainBnv.selectedItemId = R.id.navi_item_1
-        selectSolutionById(R.id.navi_item_1)
+//        selectSolutionById(R.id.navi_item_1)
+
+        val mFragments = listOf(
+            ::SolutionA,
+            ::SolutionB,
+            ::SolutionC,
+//            ::SolutionC,
+        )
+
+        vb.mainViewPager.adapter = object :
+            FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            override fun getCount(): Int = mFragments.size
+
+            override fun getItem(position: Int): Fragment {
+                return mFragments[position]()
+            }
+        }
+        vb.mainViewPager.setCurrentItem(0, false)
+        vb.mainViewPager.addOnPageChangeListener(object:ViewPager.OnPageChangeListener{
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                vb.mainBnv.selectedItemId = mTabIds[position]
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
     }
 
-    private fun selectSolutionById(itemId: Int) {
-        val fragment =
-            when (itemId) {
-                R.id.navi_item_1 -> {
-                    SolutionA()
-                }
-                R.id.navi_item_2 -> {
-                    SolutionB()
-                }
-                R.id.navi_item_3 -> {
-                    SolutionC()
-                }
-                R.id.navi_item_4 -> {
-                    SolutionC()
-                }
-                else -> {
-                    SolutionC()
-                }
-            }
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, fragment, fragment.tag)
-            .disallowAddToBackStack()
-            .commit()
-    }
+//    private fun selectSolutionById(itemId: Int) {
+//        val fragment =
+//            when (itemId) {
+//                R.id.navi_item_1 -> {
+//                    SolutionA()
+//                }
+//                R.id.navi_item_2 -> {
+//                    SolutionB()
+//                }
+//                R.id.navi_item_3 -> {
+//                    SolutionC()
+//                }
+//                R.id.navi_item_4 -> {
+//                    SolutionC()
+//                }
+//                else -> {
+//                    SolutionC()
+//                }
+//            }
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_container, fragment, fragment.tag)
+//            .disallowAddToBackStack()
+//            .commit()
+//    }
 
 
 }
--- tabbarlab/src/main/java/com/github/moqigit/tabbarlab/ui/SolutionC.kt
+++ tabbarlab/src/main/java/com/github/moqigit/tabbarlab/ui/SolutionC.kt
@@ -1,16 +1,22 @@
 package com.github.moqigit.tabbarlab.ui
 
+import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.github.moqigit.tabbarlab.R
 import com.github.moqigit.tabbarlab.base.BaseFragment
 import com.github.moqigit.tabbarlab.databinding.FragmentSolutionCBinding
+import com.github.moqigit.tabbarlab.databinding.ItemBannerBinding
 
 /**
  *
  * created by reol at 1/5/21
  */
-class SolutionC: BaseFragment<FragmentSolutionCBinding>() {
+class SolutionC : BaseFragment<FragmentSolutionCBinding>() {
     override fun getName(): String {
         return "SolutionC"
     }
@@ -20,5 +26,54 @@ class SolutionC: BaseFragment<FragmentSolutionCBinding>() {
         container: ViewGroup?
     ): FragmentSolutionCBinding {
         return FragmentSolutionCBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = BannerAdapter(arrayListOf<Int>())
+        vb.vpBanner.adapter = adapter
+        vb.vpBanner.postDelayed(
+            {
+                adapter.setNewList(listOf(
+                    R.drawable.pic_banner1,
+                    R.drawable.pic_banner2,
+                    R.drawable.pic_banner3,
+                ))
+            },
+            1000
+        )
+    }
+
+    class BannerAdapter(private val bannerList: MutableList<Int>) :
+        RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
+
+
+        class BannerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_banner, parent, false)
+        ) {
+            private var vb: ItemBannerBinding = ItemBannerBinding.bind(itemView)
+
+            fun bindView(resId: Int) {
+                vb.ivBanner.setImageResource(resId)
+                vb.tvTitle.text = "Title $resId"
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
+            return BannerViewHolder(parent)
+        }
+
+        override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
+            holder.bindView(bannerList[position])
+        }
+
+        override fun getItemCount(): Int = bannerList.size
+
+        fun setNewList(list: List<Int>){
+            bannerList.clear()
+            bannerList.addAll(list)
+            notifyDataSetChanged()
+        }
     }
 }
